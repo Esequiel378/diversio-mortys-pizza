3cf4e40686d25b28267015fc06cd0ba515a47c5d
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-05-31 21:30:00 -0300

    ⚡Add test with pytest

---
+++ test/mock.py
@@ -0,0 +1,143 @@
+from mortys_pizza import Combinations
+
+groups_1: Combinations = [
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Mayo",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        4,
+    ),
+    (
+        {
+            "crust": "Thin-Crust",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        2,
+    ),
+]
+
+groups_2: Combinations = [
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "Yes",
+            "sauce": "Mayo",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Mayo",
+            "veggies": "Broccoli",
+            "cheese": "Yes",
+        },
+        3,
+    ),
+]
+
+groups_3: Combinations = [
+    (
+        {
+            "crust": "Thin-Crust",
+            "seasoning": "No",
+            "sauce": "Mayo",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Cucumber",
+            "cheese": "Yes",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Mayo",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Broccoli",
+            "cheese": "Yes",
+        },
+        3,
+    ),
+]
+
+groups_4: Combinations = [
+    (
+        {
+            "crust": "Thin-Crust",
+            "seasoning": "No",
+            "sauce": "Mayo",
+            "veggies": "Broccoli",
+            "cheese": "No",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Cucumber",
+            "cheese": "Yes",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Broccoli",
+            "cheese": "Yes",
+        },
+        3,
+    ),
+    (
+        {
+            "crust": "Pan",
+            "seasoning": "No",
+            "sauce": "Tomato",
+            "veggies": "Broccoli",
+            "cheese": "Yes",
+        },
+        3,
+    ),
+]
---
+++ test/test_mortys_pizza.py
@@ -0,0 +1,27 @@
+import pytest
+
+from mortys_pizza import build_pizza_preference, find_pizza_combination
+
+from .mock import groups_1, groups_2, groups_3, groups_4
+
+MORTYS_PIZZA_PREFERENCE = build_pizza_preference(
+    crust="Pan", seasoning="Yes", sauce="Tomato", veggies="Broccoli", cheese="No"
+)
+
+PARAMETRIZED_GROUPS = [
+    (groups_1, groups_1[0][0]),
+    (groups_2, groups_2[1][0]),
+    (groups_3, groups_3[3][0]),
+    (groups_4, groups_4[2][0]),
+]
+
+
+@pytest.mark.parametrize("group,expected", PARAMETRIZED_GROUPS)
+def test_find_pizza_combinations(group, expected):
+    assert find_pizza_combination(group, MORTYS_PIZZA_PREFERENCE) == expected
+
+
+def test_find_pizza_combinations_with_same_id():
+    assert id(find_pizza_combination(groups_4, MORTYS_PIZZA_PREFERENCE)) == id(
+        groups_4[2][0]
+    )
